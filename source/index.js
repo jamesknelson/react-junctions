c69c0a67801af317124df35dd24d41b692f92f82
Michiel Westerbeek <happylinks@gmail.com>
2017-04-10 22:47:33 +0200

    Created Redirect component based on react-router

--- source/index.js
+++ source/index.js
@@ -1,3 +1,4 @@
 export { default as HistoryContext } from './HistoryContext'
 export { default as Link } from './Link'
 export { default as Router } from './Router'
+export { default as Redirect } from './Redirect'
